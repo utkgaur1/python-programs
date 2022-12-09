609551816d18a2500741f6facd1e65d5cc05b4b4
utkgaur1 <120082490+utkgaur1@users.noreply.github.com>
2022-12-09 21:29:31 +0530

    Add files via upload

---
+++ 1 to 10 table.py
@@ -0,0 +1,10 @@
+n=int(input("enter upto which no. u want to print the table: "))
+b=int(input("one table till which no.: "))
+for x in range(1,n+1):
+    
+    for y in range(1,b+1):
+        print(x,"*",y,"=",x*y)
+        print(" ")
+    print(" ")
+    print(" ")
+    
